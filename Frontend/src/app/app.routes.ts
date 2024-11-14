618cd9a332795b07d88f94c7b57b1c172afe2003
ElinorTch <90453919+ElinorTch@users.noreply.github.com>
2024-11-14 05:46:35 +0100

    Revert "Feat: login page done, welcome page done and house page done"

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,23 +1,3 @@
 import { Routes } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { LoginComponent } from './auth/login/login.component';
-import { HousingPageComponent } from './housing/housing-page/housing-page.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    children: [
-      { path: '', component: WelcomeComponent },
-      { path: 'house', component: HousingPageComponent },
-    ],
-  },
-  {
-    path: 'auth',
-    children: [
-      {
-        path: 'login',
-        component: LoginComponent,
-      },
-    ],
-  },
-];
+export const routes: Routes = [];
